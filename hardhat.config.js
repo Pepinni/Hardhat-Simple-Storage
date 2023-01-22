ae0094f43478b5efa41c7820572d9e067ae287e8
Piyush Verma <piyushverma476@gmail.com>
2023-01-22 12:37:44 +0530

    gas reporter using coin market cap api

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 require("./tasks/block-number");
+require("hardhat-gas-reporter");
 /** @type import('hardhat/config').HardhatUserConfig */
 
 module.exports = {
@@ -12,15 +13,23 @@ module.exports = {
             accounts: [process.env.privateKey],
             chainId: 5,
         },
-localhost : {
-	url : "http://127.0.0.1:8545/",
-	// accounts = [], hardhat placed an account for us here
-	chainId : 31337,
-	
-}
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            // accounts = [], hardhat placed an account for us here
+            chainId: 31337,
+
+        }
     },
     solidity: "0.8.17",
     etherscan: {
         apiKey: process.env.etherscanAPI,
+    },
+    gasReporter : {
+        enabled : true,
+        outputFile : "gas-report.txt",
+        noColors : true,
+        currency : "USD",
+        coinmarketcap : process.env.COIN_MKT_API,
+        token : "BITCOIN",
     }
 };
